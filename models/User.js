aa7932d4016750b459701cbbf5217d69d689187d
kingofak78 <gotakale952@gmail.com>
2025-03-17 20:49:52 -0700

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  uniqueid: { type: String, required: true, unique: true },
+  entries: [
+    {
+      name: { type: String, required: true },
+      mobileNumber: { type: String, required: true },
+      aadhar: { type: String, required: true },
+      pan: { type: String, required: true },
+      dob: { type: String, required: true },
+      submittedAt: { type: Date, default: Date.now }
+    }
+  ]
+});
+
+module.exports = mongoose.model('User', userSchema);
